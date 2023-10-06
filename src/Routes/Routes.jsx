175811cdabbec7cac6bca64e21d3c3c66181575e
afnanhussain2002 <afnanhussain2022@gmail.com>
2023-10-07 00:33:27 +0600

    added json data

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<div>Router is Ready</div>
+    }
+])
+
+export default router;
